72b41ae035dabdfc0bb0f1d3977ec8c3850281e9
Frank Hesse <45906155+metalsonic21@users.noreply.github.com>
2020-05-05 20:45:43 -0400

    i'm done dead

--- Practica TDA.sql
+++ Practica TDA.sql
@@ -3,9 +3,9 @@ CREATE OR REPLACE TYPE estadistica AS OBJECT (
     fallecidos number,
     infectados number,
     recuperados number,
-    MEMBER FUNCTION porcentaje_infectados(poblacion number, infectados number) RETURN number,
-    MEMBER FUNCTION porcentaje_fallecidos(poblacion number, infectados number, fallecidos number) RETURN number,
-    MEMBER FUNCTION porcentaje_recuperados(poblacion number, infectados number, recuperado number) RETURN number
+    MEMBER FUNCTION porcentaje_infectados RETURN number,
+    MEMBER FUNCTION porcentaje_fallecidos RETURN number,
+    MEMBER FUNCTION porcentaje_recuperados RETURN number
 );
 
 CREATE OR REPLACE TYPE BODY estadistica AS MEMBER FUNCTION porcentaje_infectados RETURN number
@@ -14,47 +14,55 @@ BEGIN
     IF infectados > poblacion THEN
         RAISE_APPLICATION_ERROR(-20001, 'Numero de infectados no puede ser mayor a la poblacion');
     ELSE
-        resultado := poblacion/infectados;
+        resultado := ROUND((infectados*100)/poblacion,2);
         return (resultado);
-    ENDIF;
+    END IF;
 END; --fin del begin
-END; --fin de la funcion
 
-CREATE OR REPLACE TYPE BODY estadistica AS MEMBER FUNCTION porcentaje_fallecidos(poblacion number, infectados number, fallecidos number) RETURN number
+MEMBER FUNCTION porcentaje_fallecidos RETURN number
 IS resultado number;
 BEGIN
-    IF fallecidos > poblacion OR fallecidos > infectados THEN
+IF fallecidos > poblacion OR fallecidos > infectados THEN
         RAISE_APPLICATION_ERROR(-20001, 'Numero de fallecidos no puede ser mayor al numero de infectados, ni el numero de fallecidos puede ser mayor a la poblacion');
     ELSE
-        resultado := poblacion/fallecidos;
+        resultado := ROUND((fallecidos*100)/infectados,2);
         return (resultado);
-    ENDIF;
-END; --fin del begin
+    END IF;
 END; --fin de la funcion
 
-CREATE OR REPLACE TYPE BODY estadistica AS MEMBER FUNCTION porcentaje_recuperados(poblacion number, infectados number, recuperados number) RETURN number
+MEMBER FUNCTION porcentaje_recuperados RETURN number 
 IS resultado number;
 BEGIN
-    IF recuperados > poblacion OR recuperados > infectados THEN
+IF recuperados > poblacion OR recuperados > infectados THEN
         RAISE_APPLICATION_ERROR(-20001, 'Numero de recuperados no puede ser mayor al numero de infectados, ni el numero de recuperados puede ser mayor a la poblacion');
     ELSE
-        resultado := poblacion/recuperados;
+        resultado := ROUND((recuperados*100)/infectados,2);
         return (resultado);
-    ENDIF;
-END; --fin del begin
-END; --fin de la funcion
-
+    END IF;
+END;
+END;
 -- Create table
 
 CREATE TABLE paises(
     id NUMBER PRIMARY KEY,
-    foto VARCHAR2(100) NOT NULL,
+    nombre VARCHAR(200) NOT NULL,
+    bandera VARCHAR2(100) NOT NULL,
     datos estadistica
 );
 
-BEGIN
-    INSERT INTO paises VALUES (1, '1.jpg', estadistica(150, 200, 10, 20)); -- fallecidos mayor a poblacion
-END;
+-- Caso donde falla
 
-SELECT TREAT(datos AS estadistica).poblacion AS poblacion FROM paises p WHERE p.id = 1;
-SELECT p.id, p.datos.poblacion as poblacion, p.datos.infectados as infectados as infectados, p.datos.fallecidos as fallecidos, p.datos.recuperados as recuperados FROM paises p WHERE p.id = 1;
+BEGIN
+    INSERT INTO paises VALUES (1, 'Alemania', '1.jpg', estadistica(150, 200, 10, 20)); -- fallecidos mayor a poblacion
+END;    
+
+SELECT p.datos.porcentaje_infectados(), p.datos.porcentaje_fallecidos() FROM paises p WHERE p.id = 1; 
+
+
+-- Caso donde funciona
+
+BEGIN
+    INSERT INTO paises VALUES (2, 'Polonia', '2.jpg', estadistica(1500, 5, 38, 13)); 
+END;    
+
+SELECT p.nombre, TO_CHAR(p.datos.porcentaje_infectados()) || '%' AS infectados, TO_CHAR(p.datos.porcentaje_fallecidos()) || '%' as "FALLECIDOS BASE A INFECTADOS", TO_CHAR(p.datos.porcentaje_recuperados()) || '%' as "RECUPERADOS BASE A INFECTADOS" FROM paises p WHERE p.id = 2; 
