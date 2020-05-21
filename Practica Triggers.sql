3d7a48f732e263800f8dfcec6cd6c175808829f3
Frank Hesse <45906155+metalsonic21@users.noreply.github.com>
2020-05-21 19:59:45 -0400

    ya funciona
    XD

--- Practica Triggers.sql
+++ Practica Triggers.sql
@@ -24,14 +24,25 @@ CREATE TABLE F_I(
     cantidad number NOT NULL
 );
 
+CREATE TABLE ordenes_compra(
+    codigo number PRIMARY KEY,
+    id_proveedor number NOT NULL,
+    id_insumo number NOT NULL,
+    id_farmacia number NOT NULL
+);
+
 ALTER TABLE F_I ADD CONSTRAINT farmacia_insumo_pk PRIMARY KEY(id_farmacia,id_insumo);
 ALTER TABLE P_I ADD CONSTRAINT proveedor_insumo_pk PRIMARY KEY(id_proveedor,id_insumo);
 
+ALTER TABLE ordenes_compra ADD CONSTRAINT orden_compra_proveedor_fk FOREIGN KEY (id_proveedor) REFERENCES proveedores(id);
+ALTER TABLE ordenes_compra ADD CONSTRAINT orden_compra_farmacia_fk FOREIGN KEY (id_farmacia) REFERENCES farmacias(id);
+ALTER TABLE ordenes_compra ADD CONSTRAINT orden_compra_insumo_fk FOREIGN KEY (id_insumo) REFERENCES insumos(id);
 ALTER TABLE F_I ADD CONSTRAINT farmacia_fi_fk FOREIGN KEY (id_farmacia) REFERENCES farmacias(id);
 ALTER TABLE F_I ADD CONSTRAINT insumo_fi_fk FOREIGN KEY (id_insumo) REFERENCES insumos(id);
 ALTER TABLE P_I ADD CONSTRAINT proveedor_pi_fk FOREIGN KEY (id_proveedor) REFERENCES proveedores(id);
 ALTER TABLE P_I ADD CONSTRAINT insumo_pi_fk FOREIGN KEY (id_insumo) REFERENCES insumos(id); 
 
+CREATE SEQUENCE ordenes_compra_codigo_seq;
 /* Farmacias */
 INSERT INTO farmacias (id,nombre) VALUES (1,'Pharma Fulla Hope');
 INSERT INTO farmacias (id,nombre) VALUES (2,'Cherry Street Pharmacy');
@@ -59,16 +70,16 @@ INSERT INTO P_I(id_proveedor,id_insumo) VALUES (3,2);
 -- Trigger
 CREATE OR REPLACE TRIGGER inventario BEFORE INSERT OR UPDATE ON F_I FOR EACH ROW
 DECLARE 
-root_prov VARCHAR2(200);
-nombre_insumo VARCHAR2(200);
-nombre_farmacia VARCHAR(200);
+root_prov number;
+num_insumo number;
+num_farmacia number;
 BEGIN
     IF :new.cantidad < 100 THEN
         -- Nombre de insumo
-        SELECT i.nombre INTO nombre_insumo FROM insumos i WHERE i.id = :new.id_insumo; 
+        SELECT i.id INTO num_insumo FROM insumos i WHERE i.id = :new.id_insumo; 
 
         -- Nombre farmacia
-        SELECT f.nombre INTO nombre_farmacia FROM farmacias f WHERE f.id = :new.id_farmacia;
+        SELECT f.id INTO num_farmacia FROM farmacias f WHERE f.id = :new.id_farmacia;
 
         -- Proveedor que entregara insumo siempre y cuando lo tenga
         SELECT * INTO root_prov
@@ -79,9 +90,10 @@ BEGIN
             ORDER BY DBMS_RANDOM.VALUE 
         )
         WHERE rownum = 1;
+
+        INSERT INTO ordenes_compra (codigo,id_proveedor,id_farmacia,id_insumo) VALUES (ordenes_compra_codigo_seq.NEXTVAL, root_prov, num_farmacia, num_insumo);
     
         :new.cantidad:= :new.cantidad+1000;
-        DBMS_OUTPUT.PUT_LINE('Se realizo una orden de compra a la farmacia ' || nombre_farmacia || 'por parte de el proveedor ' || root_prov || 'con el insumo' || nombre_insumo || '.');
     END IF;
 END;  
 
@@ -93,4 +105,16 @@ INSERT INTO F_I (id_farmacia,id_insumo,cantidad) VALUES (2,1,250);
 INSERT INTO F_I (id_farmacia,id_insumo,cantidad) VALUES (2,2,32);
 INSERT INTO F_I (id_farmacia,id_insumo,cantidad) VALUES (2,3,101);
 
+/* Consultar ordenes de compra */
+SELECT o.codigo, p.nombre as proveedor, i.nombre as insumo, f.nombre as farmacia
+FROM ordenes_compra o
+JOIN proveedores p ON p.id = o.id_proveedor
+JOIN insumos i ON i.id = o.id_insumo
+JOIN farmacias f ON f.id = o.id_farmacia;
+
+/* Consultar inventarios */
+SELECT f.nombre as farmacia, i.nombre as insumo, fi.cantidad as disponible
+FROM F_I fi
+JOIN farmacias f ON f.id = fi.id_farmacia
+JOIN insumos i ON i.id = fi.id_insumo;
 
